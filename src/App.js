c13ee0d040ff0d4351c208c39378cef87ca2d4b6
Viktoryia802 <villette@list.ru>
2020-06-12 11:58:02 +0200

    ex G number 4 done

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ function App() {
     <div>
       <Logo />
       <BestPokemon />
+      <CaughtPokemon />
     </div>
   );
 }
 
 function Logo() {
+  var appName = "Pokedex";
   return (
     <header>
-      <h1>Welcome to the Pokedex</h1>
+      <h1>Welcome to the {appName}</h1>
       <img
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="a bird"
       />
     </header>
   );
-  var { appName } = "pokedex";
 }
 
 function BestPokemon() {
   return <p>My favourite Pokemon is Squirtle</p>;
+}
+
+function CaughtPokemon() {
+  var date = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {date}</p>;
 }
 
 const Weather = () => {
